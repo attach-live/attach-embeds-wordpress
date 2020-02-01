aff69f2db441eedd2ef505f7a5eb7e33bc4ee44f
Lukas <lukky.11@seznam.cz>
2020-02-02 00:10:15 +0100

    pull translations from loco

---
+++ hooks/extract-locales.js
@@ -0,0 +1,55 @@
+// region import
+const fs = require('fs')
+const path = require('path')
+const po2json = require('po2json')
+// endregion
+
+// region helpers
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+const getDirectories = source =>
+	fs
+		.readdirSync(source)
+		.map(name => path.join(source, name))
+		.filter(isDirectory)
+		.map(path => path.split('/').pop())
+// endregion
+
+// region setup
+const [BRANCH_NAME] = process.argv.slice(2)
+const PROJECT = 'embeds-wordpress'
+// endregion
+
+// region run
+const locales = getDirectories(`po/${PROJECT}-po-archive/locales`)
+let prodError = false
+
+locales.forEach(locale => {
+	const parsed = po2json.parse(
+		fs.readFileSync(`po/${PROJECT}-po-archive/locales/${locale}/LC_MESSAGES/${PROJECT}.po`)
+	)
+
+	const translations = Object.keys(parsed)
+		.filter(Boolean)
+		.reduce((acc, val) => ((acc[val] = parsed[val].filter(Boolean).pop()), acc), {})
+
+	// ignore assets with no translations
+	const isEmpty = Object.keys(translations).every(key => translations[key] === undefined)
+	if (isEmpty) return
+
+	// validate
+	Object.keys(translations).forEach(key => {
+		if (!translations[key]) {
+			prodError = true
+			console.warn(`Missing translation: ${JSON.stringify({ locale, key })}`)
+		}
+	})
+
+	// copy
+	fs.copyFileSync(
+		`mo/${PROJECT}-mo-archive/locales/${locale}/LC_MESSAGES/${PROJECT}.mo`,
+		`attach-embeds/languages/${locale}.mo`
+	)
+})
+
+if (prodError && BRANCH_NAME.startsWith('prod')) process.exit(1)
+// endregion
