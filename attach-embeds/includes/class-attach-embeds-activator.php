9107660bcf78b8d5a28def8bee5dbcae460e250f
Oliver Hausler <oliver@closeup.cc>
2020-02-05 18:31:13 -0400

    Update default CSS

--- attach-embeds/includes/class-attach-embeds-activator.php
+++ attach-embeds/includes/class-attach-embeds-activator.php
@@ -22,8 +22,8 @@ class Attach_Embeds_Activator {
 
 		update_option('enable_reactions_first_time', 'enable', true);
 		update_option('enable_preview_first_time', 'disable', true);
-		update_option('preview_default_css', 'width: 100%;', true);
-		update_option('reaction_default_css', 'width: 100%;', true);
+		update_option('preview_default_css', 'width: 100%; padding-top: 0px; padding-left: 0px; padding-right: 0px; padding-bottom: 0px;', true);
+		update_option('reaction_default_css', 'width: 100%; padding-top: 0px; padding-left: 0px; padding-right: 0px; padding-bottom: 0px;', true);
 
 	}
 
