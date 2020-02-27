117a498ffbeee457acf9e65783dcb8c24ccea9ca
Lukas <lukky.11@seznam.cz>
2020-02-27 22:08:32 +0100

    copy themes during startup

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ set -e
 # copy languages
 cp -r /var/languages/. /var/www/html/wp-content/languages/
 
+# copy themes
+cp -r /var/themes/. /var/www/html/wp-content/themes/
+
 # run
 exec docker-entrypoint.sh apache2-foreground
